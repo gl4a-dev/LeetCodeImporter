e4c3ba09f0ccdb65d0a2750c2ce099bf375359c3
gl4a-dev <gl4aveloso.dev@gmail.com>
2026-08-12 21:16:25 -0300

    feat: generator classes adapted to use jinja2 templates

--- src/leetcode_importer/generators/base.py
+++ src/leetcode_importer/generators/base.py
@@ -1,9 +1,27 @@
 from abc import ABC, abstractmethod
 
+from jinja2 import Environment, PackageLoader
+
 from leetcode_importer.models.problem import LeetCodeProblem
 
 
 class BaseGenerator(ABC):
+    template_name: str
+
+    _environment = Environment(
+        loader=PackageLoader(
+            package_name="leetcode_importer.generators",
+            package_path="templates",
+        ),
+        autoescape=False,
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+
     @abstractmethod
     def generate(self, problem: LeetCodeProblem) -> str:
-        """Generate the file content."""
+        """Generate the source code for a problem."""
+
+    def render(self, **context) -> str:
+        template = self._environment.get_template(self.template_name)
+        return template.render(**context)
--- src/leetcode_importer/generators/python.py
+++ src/leetcode_importer/generators/python.py
@@ -1,19 +1,13 @@
+from leetcode_importer.generators.base import BaseGenerator
 from leetcode_importer.models.problem import LeetCodeProblem
 from leetcode_importer.parsers.html import html_to_text
-from leetcode_importer.generators.base import BaseGenerator
 
 
 class PythonGenerator(BaseGenerator):
+    template_name = "python.j2"
 
     def generate(self, problem: LeetCodeProblem) -> str:
-
-        description = html_to_text(problem.content_html)
-
-        return f'''"""
-{problem.id:04d}. {problem.title}
-
-{description}
-"""
-
-{problem.code}
-'''
+        return self.render(
+            problem=problem,
+            description=html_to_text(problem.content_html),
+        )
